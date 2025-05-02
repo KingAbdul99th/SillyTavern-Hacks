dca9c6df17c73cf84043ea1a12b3c2b7a138765b
KingAbdul99th <adadreigh@gmail.com>
2025-05-02 16:40:02 +0300

    create test slash command

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,59 @@ declare var SillyTavern: any;
 
 export const globalContext = SillyTavern.getContext();
 
-function test() {
-    console.log("[Hacks] Hello, World!");
-    const ctx = SillyTavern.getContext();
-    console.log(`[Hacks] ${ctx.accountStorage}`);
+function main() {
+    console.log("[Hacks] Initialize");
+
+    const SlashCommandParser = globalContext.SlashCommandParser;
+    const SlashCommand = globalContext.SlashCommand;
+    const SlashCommandNamedArgument = globalContext.SlashCommandNamedArgument;
+    const SlashCommandArgument = globalContext.SlashCommandArgument;
+    const ARGUMENT_TYPE = globalContext.ARGUMENT_TYPE;
+
+    SlashCommandParser.addCommandObject(
+        SlashCommand.fromProps(
+            {
+                name: "hacks-command",
+                callback: (namedArgs: any, unnamedArgs: any) => {
+                    return Array(namedArgs.times ?? 5)
+                        .fill(unnamedArgs.toString())
+                        .join(' ');
+                },
+                aliases: ["example-hack-command"],
+                namedArgumentList: [
+                    SlashCommandNamedArgument.fromProps({
+                        name: "times",
+                        description: "number of times to repeat the text",
+                        typeLisst: ARGUMENT_TYPE.NUMBER,
+                        defaultValue: '5',
+                    }),
+                ],
+                unnamedArgumentList: [
+                    SlashCommandArgument.fromProps({
+                        description: "the text to repeat",
+                        typeList: ARGUMENT_TYPE.STRING,
+                        isRequired: true,
+                    }),
+                ],
+                helpSString: `
+                    <div>
+                        Repeats the provided text a number of times.
+                    </div>
+                    <div>
+                        <strong>Example:</strong>
+                        <ul>
+                            <li>
+                                <pre><code class="language-stscript">/repeat foo</code></pre>
+                                returns "foofoofoofoofoo"
+                            </li>
+                            <li>
+                                <pre><code class="language-stscript">/repeat times=3 space=on bar</code></pre>
+                                returns "bar bar bar"
+                            </li>
+                        </ul>
+                    </div>
+                `
+            }));
 }
 
-test();
+main();
