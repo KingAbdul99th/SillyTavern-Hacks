05ee74d54e4cf9385b77a86b187b84627447e7ab
KingAbdul99th <adadreigh@gmail.com>
2025-05-02 16:01:59 +0300

    build

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,19 @@
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it uses a non-standard name for the exports (exports).
+(() => {
+var exports = __webpack_exports__;
+
+
+Object.defineProperty(exports, "B", ({
+  value: true
+}));
 function test() {
   console.log("Hello, World!");
 }
+test();
+})();
+
+const __webpack_exports___esModule = __webpack_exports__.B;
+export { __webpack_exports___esModule as __esModule };
 
 //# sourceMappingURL=index.js.map
