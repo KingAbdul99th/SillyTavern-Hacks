2aa610c3e4c7965958b5c5bd20521758a2b5b283
KingAbdul99th <adadreigh@gmail.com>
2025-05-11 00:39:50 +0300

    fix inline drawer

--- src/settings.tsx
+++ src/settings.tsx
@@ -8,16 +8,18 @@ const extensionConfig = {
     return (
       <>
         <div id="hacks_settings" className="extension_container">
-          <div className="inline-drawer-toggle inline-drawer-header">
-            <b>{extensionConfig.name}</b>
-            <div className="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
-          </div>
-          <div className="inline-drawer-content">
-            <div className="tracker-block flex-container">
-				      <input id="tracker_enable" type="checkbox" />
-				        <label htmlFor="tracker_enable">Enable Hacks</label>
-			      </div>
-			      <hr className="sysHR" />
+          <div className="inline-drawer">
+            <div className="inline-drawer-toggle inline-drawer-header">
+              <b>{extensionConfig.name}</b>
+              <div className="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
+            </div>
+            <div className="inline-drawer-content">
+              <div className="tracker-block flex-container">
+                <input id="tracker_enable" type="checkbox" />
+                  <label htmlFor="tracker_enable">Enable Hacks</label>
+              </div>
+              <hr className="sysHR" />
+            </div>
           </div>
         </div>
       </>
