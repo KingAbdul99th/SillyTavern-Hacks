c546ddc504cea24fb135f0e4b9f2bc890a090474
KingAbdul99th <adadreigh@gmail.com>
2025-05-12 02:13:28 +0300

    add example sass

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -58,6 +58,7 @@ module.exports = {
             },
             {
                 test: /\.s[ac]ss$/i,
+                exclude: /node_modules/,
                 use: [
                     // Creates `style` nodes from JS strings
                     "style-loader",
