101e8cfc75de29da18d9dd31ab96b280602cae3b
KingAbdul99th <adadreigh@gmail.com>
2025-05-13 17:08:38 +0300

    remove extras toggle

--- src/settings.tsx
+++ src/settings.tsx
@@ -5,39 +5,59 @@ import React, { useState } from "react";
 
 const defaultExtensionSettings = {
   name: 'KingAbdul Hacks',
-  enabled: true
+  enabled: true,
+  removeExtras: true,
 };
 
+// @ts-expect-error global extension settings
+if (!extension_settings[defaultExtensionSettings.name]) {
+// @ts-expect-error global extensions settings
+  extension_settings[defaultExtensionSettings.name] = defaultExtensionSettings;
+}
 
-function removeExtrasFromExtensionsBlock() {
-    const extensionsBlock = document.getElementById("rm_extensions_block");
-    const parent = extensionsBlock?.childNodes[1]
-    parent?.removeChild(parent.childNodes[11]);
-    parent?.removeChild(parent.childNodes[9]);
+// @ts-expect-error global extensions settings
+const extensionSettingsGlobal: defaultExtensionSettings = extension_settings[defaultExtensionSettings.name];
+
+function toggleRemoveExtrasFromExtensionsBlock() {
+  const extensionsBlock = document.getElementById("rm_extensions_block");
+  const parent = extensionsBlock?.children[1];
+  const children = parent?.children as HTMLCollectionOf<HTMLElement>;
+  const child1 = children[11];
+  const child2 = children[9];
+
+  if(extensionSettingsGlobal.removeExtras) {
+    child1.style.display = "none";
+    child2.style.display = "none";
+  }
+  else {
+    child1.style.display = "";
+    child2.style.display = "";
+  }
 }  
 
 export default function Settings() {
-  // @ts-expect-error global extensions
-  if (!extension_settings[defaultExtensionSettings.name]) {
-  // @ts-expect-error global extensions
-    extension_settings[defaultExtensionSettings.name] = defaultExtensionSettings;
-  }
-
-  // @ts-expect-error global extensions
-  const extensionSettingsGlobal: defaultExtensionSettings = extension_settings[defaultExtensionSettings.name];
-
   const [enabled, setEnabled] = useState(extensionSettingsGlobal.enabled);
+  const [removeExtras, setRemoveExtras] = useState(extensionSettingsGlobal.enabled);
 
   if(enabled) {
-    removeExtrasFromExtensionsBlock();
+    toggleRemoveExtrasFromExtensionsBlock();
   }
 
   async function handleEnabledClick() {
-    // setSettings({...extensionSettings, enabled: !extensionSettings.enabled});
     extensionSettingsGlobal.enabled = !extensionSettingsGlobal.enabled;
     console.log("enable toggled ", extensionSettingsGlobal.enabled);
     await saveSettingsDebounced();
     setEnabled(extensionSettingsGlobal.enabled);
+  }
+
+  async function handleRemoveExtrasClick() {
+    extensionSettingsGlobal.removeExtras = !extensionSettingsGlobal.removeExtras;
+    console.log("removeExtras toggled ", extensionSettingsGlobal.removeExtras);
+    await saveSettingsDebounced();
+    setRemoveExtras(extensionSettingsGlobal.removeExtras);
+    if(enabled) {
+      toggleRemoveExtrasFromExtensionsBlock();
+    }
   }
 
   return (
@@ -49,9 +69,11 @@ export default function Settings() {
             <div className="inline-drawer-icon fa-solid fa-circle-chevron-down down"></div>
           </div>
           <div className="inline-drawer-content">
-            <div className="tracker-block flex-container">
-              <input id="tracker_enable" type="checkbox" onClick={handleEnabledClick} checked={enabled}/>
-                <label htmlFor="tracker_enable">Enable Hacks</label>
+            <div className="hacks-block flex-container">
+              <input id="hacks-enable" type="checkbox" onClick={handleEnabledClick} checked={enabled}/>
+              <label htmlFor="hacks-enable">Enable Hacks</label>
+              <input id="hacks-remove-extras" type="checkbox" onClick={handleRemoveExtrasClick} checked={removeExtras}/>
+              <label htmlFor="hacks-remove-extras">Remove Extras Block</label>
             </div>
             <hr className="sysHR" />
           </div>
